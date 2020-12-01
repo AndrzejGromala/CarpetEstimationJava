766d9bd8a6e18ce10303bdd9f8f8917555d5e308
zdzislaw dyrman <zdzislawdyrman1@gmail.com>
2020-12-01 22:48:10 +0000

    CarpetEstimation verified

---
+++ carpetProgram/src/carpetprogram/classVariables.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carpetprogram;
+
+/**
+ *
+ * @author Andrzej
+ */
+public class classVariables {
+    
+    //double a=25.99;
+    //double b=22.99;
+    //double c=19.99;
+    //double d=15.99;
+    
+    int length=0;
+    double carpetPrice=0;
+    double fittingCharge=0;
+    double subTotal=0;
+    double total=0;
+    
+    //char item1='A', item2='B', intem3='C', item4='D';
+    
+    
+   
+}
